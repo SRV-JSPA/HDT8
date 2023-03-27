3e9f4c0db627c890dc281622820d985c33bd8205
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-03-27 10:49:49 -0600

    interfaz

--- InterfazQueue.java
+++ InterfazQueue.java
@@ -2,9 +2,5 @@ public interface interfazQueue <E extends Comparable<E>> {
     public E getFirst();
     public E remove ();
     public void add( E value);
-    public boolean isEmpty ();
-    public int size();
-    public void clear ();
-
 
 }
