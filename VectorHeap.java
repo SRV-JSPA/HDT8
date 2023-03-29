53c42432c61dea4fbea8e2c69b6906100a4cfb9c
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-03-29 07:18:24 -0600

    correccion clase heap e interfaz

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,5 +1,20 @@
 import java.util.*;
 
-public class VectorHeap <E extends Comparable<E>> implements iqueue{
-    
+public class VectorHeap <E extends Comparable<E>> implements iqueue<E>{
+
+    @Override
+    public E getFirst(){
+        return null;
+    }
+
+    @Override
+    public E remove(){
+        return null;
+    }
+
+    @Override
+    public void agregar(E v){
+
+    }
 }
+
--- iqueue.java
+++ iqueue.java
@@ -1,5 +1,5 @@
 public interface iqueue <E extends Comparable<E>> {
     public E getFirst();
     public E remove();
-    public void add(E value);
+    public void agregar(E v);
 }
