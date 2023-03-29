19e384488d7a24eb78bfd6963b9ece8705a92c54
Santiago Pereira <per22318@uvg.edu.gt>
2023-03-29 13:46:14 -0600

    vector heap

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
 public class VectorHeap <E extends Comparable<E>> implements iqueue<E>{
-
+    
     @Override
     public E getFirst(){
         return null;
     }
 
     @Override
-    public E remove(){
+    public E remove (){
         return null;
     }
 
     @Override
-    public void agregar(E v){
-
+    public void agregar (E v){
+        
     }
 }
-
