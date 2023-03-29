2edf092027a1bd92e2dfa9d02ca424cf6433d849
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-03-29 07:06:31 -0600

    <3

--- ChichicasteLinux.java
+++ ChichicasteLinux.java
@@ -1,5 +1,49 @@
 import java.util.*;
 
 public class ChichicasteLinux {
+    private String Nproceso;
+    private String Nusuario;
+    private int Nice;
+    private int prioridad;
+    
+    public String getNproceso() {
+        return Nproceso;
+    }
+
+    public void setNproceso(String nproceso) {
+        Nproceso = nproceso;
+    }
+
+    public String getNusuario() {
+        return Nusuario;
+    }
+
+    public void setNusuario(String nusuario) {
+        Nusuario = nusuario;
+    }
+
+    public int getNice() {
+        return Nice;
+    }
+
+    public void setNice(int nice) {
+        Nice = nice;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public ChichicasteLinux(String nproceso, String nusuario, int nice, int prioridad) {
+        Nproceso = nproceso;
+        Nusuario = nusuario;
+        Nice = nice;
+        this.prioridad = prioridad;
+    }
+
 
 }
