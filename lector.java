13a863296ddf9b8eebe13dfe124f156267179d06
Santiago Pereira <per22318@uvg.edu.gt>
2023-03-29 20:00:24 -0600

    Update lector.java

--- lector.java
+++ lector.java
@@ -1,4 +1,10 @@
-import java.util.*;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class lector {
     
