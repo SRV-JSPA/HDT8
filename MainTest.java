b7e1855c80993cd5abdeac9518a93982b897ba0f
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-04-01 18:30:58 -0600

    Update pruebas

---
+++ MainTest.java
@@ -0,0 +1,9 @@
+import java.io.IOError;
+import java.io.IOException;
+
+public class MainTest {
+    public static void main(String[] args) throws IOException {
+        Test t = new Test();
+        t.pruebavector();
+    }
+}
--- Test.java
+++ Test.java
@@ -2,13 +2,9 @@ import java.util.*;
 
 public class Test {
 
-    public static void main(String[] args) {
-        
-    }
-
-    public static void pruebavector(){
+    public void pruebavector(){
         VectorHeap<Integer> pruebaVector=new VectorHeap<Integer>();
-
+        pruebaVector.percolateUp(30);
         pruebaVector.add(10);
         pruebaVector.pushDownRoot(40);
     }
