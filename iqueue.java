b2d3a333c2a4b6dbe9208e79989b7f97158fe0c4
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-04-01 18:44:19 -0600

    Encabezados

--- iqueue.java
+++ iqueue.java
@@ -1,3 +1,14 @@
+/*
+Profesor: Douglas Leonel Barrios
+Auxiliares: Fernanda Esquivel y Francisco Castillo
+INTEGRANTES: 
+José Santiago Pereira Alvarado, 22318
+Nancy Gabriela Mazariegos Molina, 22513
+27/01/2023
+Hoja de Trabajo 8
+Curso: Algoritmos y Estructuras de datos
+Sección: 20
+*/
 public interface iqueue <E extends Comparable<E>> {
     public E getFirst();
     public E remove();
