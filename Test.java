21fbcbc6fad36c0d62cabcc5aedf1d2b5b01fd7d
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-04-01 18:21:04 -0600

    pruebas

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void pruebavector(){
+        VectorHeap<Integer> pruebaVector=new VectorHeap<Integer>();
+
+        pruebaVector.add(10);
+        pruebaVector.pushDownRoot(40);
+    }
+}
