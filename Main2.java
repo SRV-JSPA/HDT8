e28fa959e88cd095ba8de85b3aa18f07a78418cb
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-04-01 22:32:59 -0600

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,8 @@
+import java.util.PriorityQueue;
+public class Main2 {
+    public static void main(String[] args) {
+        
+    lector r = new lector();
+    iqueue<ChichicasteLinux> cp = new iqueue<>(r.getPR());
+    }
+}
