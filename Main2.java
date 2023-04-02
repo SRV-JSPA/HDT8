b138bebd59dd8bb64ba0f57d4c4e7724ccf4f8c2
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-04-01 22:53:09 -0600

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,8 +1,19 @@
 import java.util.PriorityQueue;
+
+import javax.sound.midi.Soundbank;
 public class Main2 {
     public static void main(String[] args) {
         
     lector r = new lector();
-    PriorityQueue<ChichicasteLinux> colaProcesos = new PriorityQueue<>(r.getPR());
+    PriorityQueue<ChichicasteLinux> cp = new PriorityQueue<>(r.getPR());
+    ChichicasteLinux pa = cp.poll();
+
+    String x = String.format("%-10s %-10s %-10s\n",pa.getNproceso(),pa.getNusuario(),pa.getNice());
+    System.out.println(x);
+
+    for (ChichicasteLinux chichicasteLinux : cp){
+        String y = String.format("%-10s %-10s %-10s %-10s\n",pa.getNproceso(),pa.getNusuario(),pa.getNice(), pa.getPrioridad());
+        System.out.println(y);
+    }
     }
 }
